3fbf1d43be04bb6f7e627a85ced127db05fa5cdd
Julien Faure <julien.faure@iaca-electronique.com>
2025-04-23 16:53:10 +0200

    Remove redundant echo statements and unnecessary environment variable exports in deployment script and CI configuration

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo ${SFTP_HOST} $SFTP_HOST
-echo toto
 sshpass -p "${SFTP_PASSWORD}" sftp -o StrictHostKeyChecking=no -P ${SFTP_PORT} "${SFTP_USER}@${SFTP_HOST}" <<EOL
 cd ${REPO_PATH}
 put ${PACKAGE_NAME}
