551e154afeb9390c761604595f5d6153aacbaed0
Julien Faure <julien.faure@iaca-electronique.com>
2025-04-23 16:48:51 +0200

    Another test CI/CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo ${SFTP_HOST} $SFTP_HOST
+echo toto
 sshpass -p "${SFTP_PASSWORD}" sftp -o StrictHostKeyChecking=no -P ${SFTP_PORT} "${SFTP_USER}@${SFTP_HOST}" <<EOL
 cd ${REPO_PATH}
 put ${PACKAGE_NAME}
